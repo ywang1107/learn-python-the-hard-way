39268e04d92633cd206a173f73293d7874e7e31f
Yicheng Wang <ywang1107@gmail.com>
2019-03-06 00:27:55 -0600

    exercise 4-16

---
+++ ex15.py
@@ -0,0 +1,15 @@
+from sys import argv
+
+script, filename = argv
+
+txt = open(filename)
+
+print(f"Here's your file {filename}:")
+print(txt.read())
+
+print("Type the filename again:")
+file_again = input("> ")
+
+txt_again = open(file_again)
+file_again.close()
+print(txt_again.read())
